337722c9b08a92f6235a8088176319f2f5da7649
mohamedAhmed972001 <mezo1927061@gmail.com>
2024-12-13 02:32:30 +0200

    hello

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-require('dotenv').config();
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-});
