b7138d88b9392c9e12a500889fc957fa8555f810
Mohamed Ahmed <74019733+mohamedAhmed972001@users.noreply.github.com>
2024-12-14 03:34:14 +0200

    Update authService.js

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -8,45 +8,35 @@ const createToken = require('../utils/createToken');
 const User = require('../models/userModel');
 
 // @desc    Signup
-// @route   GET /api/v1/auth/signup
-// @access  Public
 exports.signup = asyncHandler(async (req, res, next) => {
-  // 1- Create user
   const user = await User.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
   });
-
-  // 2- Generate token
   const token = createToken(user._id);
 
   res.status(201).json({ data: user, token });
 });
 
 // @desc    Login
-// @route   GET /api/v1/auth/login
-// @access  Public
 exports.login = asyncHandler(async (req, res, next) => {
-  // 1) check if password and email in the body (validation)
-  // 2) check if user exist & check if password is correct
+
   const user = await User.findOne({ email: req.body.email });
 
   if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
     return next(new ApiError('Incorrect email or password', 401));
   }
-  // 3) generate token
   const token = createToken(user._id);
 
-  // Delete password from response
+
   delete user._doc.password;
-  // 4) send response to client side
+
   res.status(200).json({ data: user, token });
 });
 
 // @desc   make sure the user is logged in
 exports.protect = asyncHandler(async (req, res, next) => {
-  // 1) Check if token exist, if exist get
   let token;
   if (
     req.headers.authorization &&
@@ -63,10 +53,8 @@ exports.protect = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // 2) Verify token (no change happens, expired token)
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
-  // 3) Check if user exists
   const currentUser = await User.findById(decoded.userId);
   if (!currentUser) {
     return next(
@@ -77,13 +65,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // 4) Check if user change his password after token created
+
   if (currentUser.passwordChangedAt) {
     const passChangedTimestamp = parseInt(
       currentUser.passwordChangedAt.getTime() / 1000,
       10
     );
-    // Password changed after token created (Error)
     if (passChangedTimestamp > decoded.iat) {
       return next(
         new ApiError(
@@ -114,10 +101,8 @@ exports.allowedTo = (...roles) =>
 
 
 // @desc    Reset password
-// @route   POST /api/v1/auth/resetPassword
-// @access  Public
 exports.resetPassword = asyncHandler(async (req, res, next) => {
-  // 1) Get user based on email
+
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
     return next(
@@ -125,7 +110,7 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
     );
   }
 
-  // 2) Check if reset code verified
+
   if (!user.passwordResetVerified) {
     return next(new ApiError('Reset code not verified', 400));
   }
@@ -137,7 +122,7 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
 
   await user.save();
 
-  // 3) if everything is ok, generate token
+
   const token = createToken(user._id);
   res.status(200).json({ token });
 });
