dd5f480636f41b55ee0e2525061709d295088f43
Abdelrahman <a.rashed1212@yahoo.com>
2024-12-15 12:13:16 +0200

    add new book route

---
+++ backend/# Code Citations.md
@@ -0,0 +1,46 @@
+# Code Citations
+
+## License: unknown
+https://github.com/akibrot/ET-HBackEndProject/tree/da5863fc99aeea68e8380585cf9fc98ce624bc45/ppxtpackage-lock
+
+```
+"requires": true,
+  "packages": {
+    "": {
+      "name": "backend",
+      "version": "1.0.0",
+      "license": "ISC",
+      "dependencies": {
+        "bcrypt": "^5
+```
+
+
+## License: unknown
+https://github.com/Droot22/case1_CRUDeventos-/tree/e0a27eaf0a0562e3c1797319f4e2a91460c60836/backend
+
+```
+1.0.0",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "backend",
+      "version": "1.0.0",
+      "license": "ISC"
+```
+
+
+## License: unknown
+https://github.com/atharv-31/movie-booking/tree/c9a56bba9a4afb61445d2f286cd76c0aca5d664b/package-lock
+
+```
+backend",
+  "version": "1.0.0",
+  "lockfileVersion": 3,
+  "requires": true,
+  "packages": {
+    "": {
+      "name": "backend",
+      "version": "1.0.0"
+```
+
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Book = require('../models/book.model');
 const isAdmin = require('../middleware/Admin');
 const asyncHandler = require('express-async-handler'); 
+const BookValidation = require('../validation/books.validate');
 const router = express.Router();
 
 // Create a new book
@@ -9,4 +10,21 @@ router.get('/allbooks' , Admin , asyncHandler(async(req , res) => {
     const books = await Book.find({});
     res.json(books);
 }));
-//add new book
+//add new book (use book.model) and use admin middleware to let admin add new book
+router.post('/addbook/:id', Admin ,asyncHandler(async(req , res) => {
+    const {error} = BookValidation.validate(req.body);
+        if(error) return res.status(400).json({massege: error.details[0].message});
+    const book = new Book({
+        title: req.body.title,
+        slug: req.body.slug,
+        description: req.body.description,
+        quantity: req.body.quantity,
+        sold: req.body.sold,
+        price: req.body.price,
+        priceAfterDiscount: req.body.priceAfterDiscount,
+        imageCover: req.body.imageCover
+    });
+    const newbook =await book.save();
+    res.status(201).send(newbook);
+    res.status(500).send('internal server error');
+}));
