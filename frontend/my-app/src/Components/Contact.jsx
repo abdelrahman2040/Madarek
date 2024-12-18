1b2c8d0be739ff7f23b6b9c8c76bb84644074740
B A D R <badrrbadrr92@gmail.com>
2024-12-18 20:35:11 +0200

    improve contact page and style

--- frontend/my-app/src/Components/Contact.jsx
+++ frontend/my-app/src/Components/Contact.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import './Contact.css';
-// import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
+import chatImage from '../Assets/chat.png';
+import visitUs from '../Assets/location-mark.png';
+import callUs from '../Assets/phone-call.png';
+import facebook_logo from '../Assets/facebook.png';
+import linkedin_logo from '../Assets/linkedin.png';
+import twitter_logo from '../Assets/twitter.png';
+import youtube_logo from '../Assets/youtube.png';
+import instagram_logo from '../Assets/instagram.png';
 
 function Contact() {
     const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
     const [topic, setTopic] = useState("");
+    const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
 
     const handleSubmit = (e) => {
@@ -18,29 +25,58 @@ function Contact() {
         <div className="contact-wrapper">
           <div className="contact-container">
             <div className="contact-info">
-              <h2>Chat to us</h2>
-              <p>Our friendly team is here to help.</p>
-              <a href="mailto:hi@our.com">hi@untitledui.com</a>
-    
-              <h2>Visit us</h2>
-              <p>Come say hello at our office HQ.</p>
-              <p>100 Smith Street<br />Collingwood VIC 3066 AU</p>
-    
-              <h2>Call us</h2>
-              <p>Mon-Fri from 8am to 5pm.</p>
-              <p>+1 (000) 000-0000</p>
+              <div className="chat-to-us">
+                <div className="container-img">
+                  <img src={chatImage} alt="Chat Us" />
+                </div>
+                <h2>Chat to us</h2>
+                <p>Our friendly team is here to help.</p>
+                <a href="mailto:hi@our.com">hi@untitledui.com</a>
+              </div>
+
+              <div className="visit-us">
+                <div className="container-img">
+                  <img src={visitUs} alt="Visit Us" />
+                </div>
+                <h2>Visit us</h2>
+                <p>Come say hello at our office HQ.</p>
+                <p>100 Smith Street<br />Collingwood VIC 3066 AU</p>
+              </div>
+
+              <div className="call-us">
+                <div className="container-img">
+                  <img src={callUs} alt="Call Us" />
+                </div>
+                <h2>Call us</h2>
+                <p>Mon-Fri from 8am to 5pm.</p>
+                <p>+1 (000) 000-0000</p>
+              </div>
+
+              <div className="social-icons">
+                <img src={facebook_logo} alt="facebook" />
+                <img src={instagram_logo} alt="instagram" />
+                <img src={linkedin_logo} alt="linkedin-logo" />
+                <img src={youtube_logo} alt="facebook" />
+                <img src={twitter_logo} alt="facebook" />
+                {/* <FaFacebook size={25} />
+                <FaInstagram size={25}  />
+                <FaLinkedin size={25} />
+                <FaYoutube size={25} />
+                <FaTwitter size={25} /> */}
+              </div>
+
             </div>
             <div className="contact-form">
               <h2>Got ideas? We've got the skills. Let's team up.</h2>
               <p>Tell us more about yourself and what you're got in mind.</p>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <div className="form-fields">
-                    <input type="text" placeholder="Your name" required />
-                    <input type="email" placeholder="you@company.com" required />
-                    <textarea placeholder="Tell us a little about the project..." required></textarea>
+                    <input type="text" placeholder="Your name" value={name} onChange={(e) => setName(e.target.value)} required />
+                    <input type="email" placeholder="you@company.com" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                    <textarea placeholder="Tell us a little about the project..." value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
                 </div>
                 <div className="form-options">
-                  <label><input type="checkbox" name="service" value="our-products" /> Our Products</label>
+                  <label><input type="checkbox" name="service" value="Our-Prroducts" /> Our Products</label>
                   <label><input type="checkbox" name="service" value="bugs" /> Bugs</label>
                   <label><input type="checkbox" name="service" value="user-research" /> User research</label>
                   <label><input type="checkbox" name="service" value="strategy" /> Strategy &amp; consulting</label>
